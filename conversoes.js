d6d071f23302acc5474ba9ed4f0b6abf8ea6b808
flavio <zannoniart@gmail.com>
2023-02-16 10:27:39 -0300

    flavio

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+//conversão implicita
+
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + Number(numeroString))
+
+console.log()
+
+//conversão explicita
+
+//Number()
+//String()
+
