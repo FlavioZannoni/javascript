58f7b816c4c6aedf378d234f64b26c578db17e5c
flavio <zannoniart@gmail.com>
2023-02-16 10:21:31 -0300

    codigo var e conversoes

---
+++ true-false.js
@@ -0,0 +1,15 @@
+//true-false.js
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+let minhaVar;
+let varNull = null;
+
+//console.log(0 == false) 
+
+let numerto = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
